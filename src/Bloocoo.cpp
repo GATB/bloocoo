a32b87609f7795738f4135eb231579bc3616f1aa
Guillaume Rizk <guillaume.rizk@gmail.com>
2013-07-11 15:31:43 +0200

    dont correct N

--- src/Bloocoo.cpp
+++ src/Bloocoo.cpp
@@ -923,6 +923,8 @@ int Bloocoo::apply_correction(char *readseq, int pos, int good_nt){
 	}
 	char bin2NT[4] = {'A','C','T','G'};
 	
+     if(readseq[pos] =='N') return 0; // dont correct N ?
+    
 	std::ostringstream oss;
     oss << _seq_num;
     std::string str__seq_num = oss.str();
