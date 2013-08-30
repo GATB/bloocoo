b63e8e99217f6ba5507c65fea97696073a07afe6
gaetan benoit <gbenoit@orvet.irisa.fr>
2013-08-30 17:29:17 +0200

    Bloocoo test : correction softwares
    Former-commit-id: 15916d77a964b4e0729a117e1bf57426a91b93f0

--- test/bloocootest/job.bash
+++ test/bloocootest/job.bash
@@ -5,6 +5,10 @@
 #$ -m bea
 #$ -cwd 
 
+#for quake
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:~/workspace/gatb-tools/gatb-tools/tools/bloocoo/test/bloocootest/quake/jellyfish/lib
+export PKG_CONFIG_PATH=${PKG_CONFIG_PATH}:~/workspace/gatb-tools/gatb-tools/tools/bloocoo/test/bloocootest/quake/jellyfish/lib/pkgconfig
+
 source /local/env/envR
 
 cd jobs/job1
--- test/bloocootest/jobs/job1/test.py
+++ test/bloocootest/jobs/job1/test.py
@@ -8,11 +8,11 @@ from libs.TestExec import *
 #variables parametrables (valeur par defaut)
 
 p = {
-	"genome_filename": "",
+	"genome_filename": "ecoli.fasta",
 	"genome_size": 10000,
 	"reads_size": 100,
 	"cover": 30,
-	"kmer_size": 30,
+	"kmer_size": 24,
 	"bloom_threshold": 4,
 	"error_rate": 0.01,
 	"result_filename_prefix": "test",
@@ -21,7 +21,7 @@ p = {
 	"nb_kmer_checked": 8,
 	"min_nb_kmer_checked": 8,
 	"regenerate_reads": True,
-	"correction_software": 0, 
+	"correction_software": 1, 
 	"read_format":"fasta"
 }
 
