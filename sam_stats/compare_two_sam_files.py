1877790038a029e434ff01684efecf6ef9c87382
Rayan <chikhi@psu.edu>
2015-07-30 22:26:32 +0200

    fixed compare_two_sams, it's good now
    Former-commit-id: 3b7ef9eab80c218d15fba125a440f1360fdea46e

--- sam_stats/compare_two_sam_files.py
+++ sam_stats/compare_two_sam_files.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# requires: python >=2.7, pysam and pyfasta (e.g. pip install --user pysam pyfasta) if it fails, read this: http://stackoverflow.com/questions/20585218/install-python-package-without-root-access
+# requires: python >=2.7, pysam and pyfasta>=0.8 (e.g. pip install --user pysam pyfasta) if it fails, read this: http://stackoverflow.com/questions/20585218/install-python-package-without-root-access
+# also on genocluster, run with "python -S" else it will take the machine pyfasta (older version, not compatible)
 
 import pysam
 import pyfasta
@@ -13,8 +14,8 @@ if len(sys.argv) < 4:
 def open_sambam(file):
     return pysam.Samfile(file, "rb" if file.endswith("bam") else "r")#, check_header = False, check_sq = False)
 
-mapped_reads_uncorrected = open_sambam(sys.argv[1])
-mapped_reads_corrected = open_sambam(sys.argv[2])
+mapped_reads_corrected = open_sambam(sys.argv[1])
+mapped_reads_uncorrected = open_sambam(sys.argv[2])
 reference = pyfasta.Fasta(sys.argv[3], key_fn=lambda key: key.split()[0]) #get before the first space
 
 print "reference number of sequences",len(reference)
@@ -34,15 +35,16 @@ def get_mismatches_tuples(r, refname):
 cor_read_counter = 0
 nb_bases_in_cor_reads, nb_bases_in_uncor_reads = 0, 0
 good_corrections, bad_corrections = 0,0
+better_reads, worse_reads = 0,0
 uncor_iterator = mapped_reads_uncorrected
 
 
 def report():
     print "---"
     print "done, %d corrected reads processed" % cor_read_counter
-    print "number of bases in corrected/uncorrected reads:",nb_bases_in_cor_reads,"/",nb_bases_in_uncor_reads
-    print "good corrections", good_corrections
-    print "bad corrections", bad_corrections
+    print "number of bases in corrected/uncorrected reads:",nb_bases_in_cor_reads,"/", nb_bases_in_uncor_reads
+    print "good/bad base corrections", good_corrections, "/", bad_corrections
+    print "better/worse reads", better_reads, "/", worse_reads
     print "---"
 
 
@@ -58,16 +60,25 @@ for cor_read in mapped_reads_corrected:
         report()
 
     # synchronine with uncorrected reads
-    uncor_read = uncor_iterator.next()
-    while uncor_read.alen is None:
+    try:
         uncor_read = uncor_iterator.next()
-    nb_bases_in_uncor_reads += uncor_read.alen 
-    while cor_read.qname != uncor_read.qname:
-        uncor_read = uncor_iterator.next()
-        while uncor_read.alen is None:
+        if uncor_read.alen is not None:
+            nb_bases_in_uncor_reads += uncor_read.alen 
+        skip=0
+        while cor_read.qname != uncor_read.qname:
             uncor_read = uncor_iterator.next()
-        nb_bases_in_uncor_reads += uncor_read.alen 
+            if uncor_read.alen is not None:
+                nb_bases_in_uncor_reads += uncor_read.alen 
+            skip += 1
+        if skip:
+            print "skipped",skip,"uncorrected reads"
+    except StopIteration:
+        print "end of uncorr reads (skip %d, nb cor reads processed %d, was looking for cor read id %s)" % (skip, cor_read_counter, cor_read.qname)
+        sys.exit(1) 
     #assert(cor_read.qname == uncor_read.qname)
+    
+    if uncor_read.alen is None:
+        continue
 
     ref_end = cor_read.aend # last base of the read + 1
     if ref_end is None:
@@ -76,10 +87,22 @@ for cor_read in mapped_reads_corrected:
     ref_start = ref_end - cor_read.alen + 1 # first base of the read
     #assert(ref_start == cor_read.pos)
 
+    # try to make sure both the corrected and uncorrected reads map at the same place
+    if cor_read.reference_id != uncor_read.reference_id:
+        continue
+    if abs(cor_read.aend - uncor_read.aend) > cor_read.alen:
+        continue
+
     refname = mapped_reads_corrected.getrname(cor_read.reference_id)
     cor_mismatches = get_mismatches_tuples(cor_read, refname)
     uncor_mismatches = get_mismatches_tuples(uncor_read, refname)
     good_corrections += len(uncor_mismatches - cor_mismatches)
-    bad_corrections += len(cor_mismatches - uncor_mismatches)
+    bad_corrections += len(cor_mismatches - uncor_mismatches)    
+    #print cor_mismatches - uncor_mismatches, cor_mismatches, uncor_mismatches, cor_read.aend, uncor_read.aend
+
+    if len(cor_mismatches) < len(uncor_mismatches):
+        better_reads += 1
+    elif len(cor_mismatches) > len(uncor_mismatches):
+        worse_reads += 1
 
 report()
