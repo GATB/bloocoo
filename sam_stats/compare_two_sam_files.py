c020705d37c2a3690a0e56de876dcfdde1bc5394
Rayan <chikhi@psu.edu>
2015-07-30 16:47:15 +0200

    committing comparison script but it's probably not reporting good number of bad corrections yet
    Former-commit-id: 1c38cc20c925cf35193acf79b4828933d49e2f8f

--- sam_stats/compare_two_sam_files.py
+++ sam_stats/compare_two_sam_files.py
@@ -10,9 +10,12 @@ import sys
 if len(sys.argv) < 4:
     exit("args: cor.sam uncor.sam ref.fa")
 
-mapped_reads_uncorrected =  pysam.Samfile(sys.argv[1],"r")
-mapped_reads_corrected =  pysam.Samfile(sys.argv[2],"r")
-reference = pyfasta.Fasta(sys.argv[3])
+def open_sambam(file):
+    return pysam.Samfile(file, "rb" if file.endswith("bam") else "r")#, check_header = False, check_sq = False)
+
+mapped_reads_uncorrected = open_sambam(sys.argv[1])
+mapped_reads_corrected = open_sambam(sys.argv[2])
+reference = pyfasta.Fasta(sys.argv[3], key_fn=lambda key: key.split()[0]) #get before the first space
 
 print "reference number of sequences",len(reference)
 print "initialization done"
@@ -25,18 +28,25 @@ def parse_cigar(cigar,f):
     return sum([y for x,y in cigar if correspondence[f] == x] + [0])
 
 def get_mismatches_tuples(r, refname):
-    print reference[refname]
-    print r.query_sequence
-    print r.get_aligned_pairs(matches_only=True)
-    print  set(filter(lambda (x,y): r.query_sequence[x] != reference[refname][y], r.get_aligned_pairs(matches_only=True)))
+    # return M tuples where the ref nucleotide doesnt match the read nucleotide (i.e. sequencing errors or snps)
     return set(filter(lambda (x,y): r.query_sequence[x] != reference[refname][y], r.get_aligned_pairs(matches_only=True)))
 
 cor_read_counter = 0
 nb_bases_in_cor_reads, nb_bases_in_uncor_reads = 0, 0
 good_corrections, bad_corrections = 0,0
-uncor_iterator = mapped_reads_uncorrected.fetch()
+uncor_iterator = mapped_reads_uncorrected
 
-for cor_read in mapped_reads_corrected.fetch():
+
+def report():
+    print "---"
+    print "done, %d corrected reads processed" % cor_read_counter
+    print "number of bases in corrected/uncorrected reads:",nb_bases_in_cor_reads,"/",nb_bases_in_uncor_reads
+    print "good corrections", good_corrections
+    print "bad corrections", bad_corrections
+    print "---"
+
+
+for cor_read in mapped_reads_corrected:
     if cor_read.alen is None:
         continue
     nb_bases_in_cor_reads += cor_read.alen 
@@ -44,32 +54,32 @@ for cor_read in mapped_reads_corrected.fetch():
 
     if cor_read_counter % 500000 == 0:
         print "%d corrected reads processed" % cor_read_counter
+        print "so far",
+        report()
 
     # synchronine with uncorrected reads
     uncor_read = uncor_iterator.next()
+    while uncor_read.alen is None:
+        uncor_read = uncor_iterator.next()
     nb_bases_in_uncor_reads += uncor_read.alen 
     while cor_read.qname != uncor_read.qname:
-        print uncor_read.qname, cor_read.qname
         uncor_read = uncor_iterator.next()
+        while uncor_read.alen is None:
+            uncor_read = uncor_iterator.next()
         nb_bases_in_uncor_reads += uncor_read.alen 
-    assert(cor_read.qname == uncor_read.qname)
+    #assert(cor_read.qname == uncor_read.qname)
 
     ref_end = cor_read.aend # last base of the read + 1
     if ref_end is None:
         continue
     ref_end -= 1
     ref_start = ref_end - cor_read.alen + 1 # first base of the read
-    assert(ref_start == cor_read.pos)
+    #assert(ref_start == cor_read.pos)
 
     refname = mapped_reads_corrected.getrname(cor_read.reference_id)
     cor_mismatches = get_mismatches_tuples(cor_read, refname)
     uncor_mismatches = get_mismatches_tuples(uncor_read, refname)
-    print cor_mismatches
     good_corrections += len(uncor_mismatches - cor_mismatches)
-    bad_corrections = len(cor_mismatches - uncor_mismatches)
+    bad_corrections += len(cor_mismatches - uncor_mismatches)
 
-print "done, %d corrected reads processed" % cor_read_counter
-print "number of bases in corrected/uncorrected reads:",nb_bases_in_cor_reads,"/",nb_bases_in_uncor_reads
-print "---"
-print "good corrections", good_corrections
-print "bad corrections", bad_corrections
+report()
